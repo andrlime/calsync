3ea47f2dbcf568748c6d8445b2c34d7601d9081b
Andrew Li <andrewli@u.northwestern.edu>
2025-09-19 09:40:16 -0500

    clean up bucket events code

--- calsync/core/event/bucket_events.py
+++ calsync/core/event/bucket_events.py
@@ -6,19 +6,40 @@ from calsync.util.logger import create_logger
 logger = create_logger()
 
 
-class BucketEvents:
-    def __init__(self, events: list[event.T], days: list[arrow.Arrow]):
+class EventStore_:
+    def __init__(self) -> None:
         self.store: dict[str, list[event.T]] = {}
-        for d in days:
-            self.store[d.format("YY-MMDD-dddd")] = []
-        for e in events:
-            key = e.rawday.format("YY-MMDD-dddd")
-            if key not in self.store:
-                self.store[key] = []
-            self.store[key].append(e)
+
+    def get_date_string(self, time: arrow.Arrow) -> str:
+        return time.format("YY-MMDD-dddd")
+
+    def create_bucket(self, time: arrow.Arrow) -> str:
+        key = self.get_date_string(time)
+        if key not in self.store:
+            self.store[key] = []
+        return key
+
+    def add_event(self, e: event.T) -> bool:
+        key = self.create_bucket(e.rawday)
+        self.store[key].append(e)
+        return True
+
+    def sort_buckets(self) -> bool:
         for bucket in self.store.values():
             bucket.sort(key=lambda e: e.starttime)
+        return True
+
+    def get(self) -> dict[str, list[event.T]]:
+        return self.store
+
+
+class BucketEvents:
+    def __init__(self, events: list[event.T], days: list[arrow.Arrow]) -> None:
+        self.store = EventStore_()
+        [self.store.create_bucket(d) for d in days]
+        [self.store.add_event(e) for e in events]
+        self.store.sort_buckets()
 
     def filt(self) -> dict[str, list[event.T]]:
         logger.info("Bucket events ok")
-        return self.store
+        return self.store.get()
