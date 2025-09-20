91b939268276095861d0266fbcfd1c5f8d09a0c4
Andrew Li <andrewli@u.northwestern.edu>
2025-09-19 20:07:04 -0500

    run formatter

--- calsync/core/event/event_types.py
+++ calsync/core/event/event_types.py
@@ -2,14 +2,18 @@ from dataclasses import dataclass
 
 import arrow
 
+
 def create_checkbox_(contents: str, checkbox_type: str = " ") -> str:
     return f"- [{checkbox_type}] {contents}"
+
 
 def create_location_checkbox_(contents: str) -> str:
     return create_checkbox_(contents, "l")
 
+
 def create_star_checkbox_(contents: str) -> str:
     return create_checkbox_(contents, "*")
+
 
 @dataclass
 class T:
@@ -32,6 +36,14 @@ class T:
 
     def to_obsidian_string(self) -> str:
         checkbox_type = "!" if self.isimportant else "<"
-        location = ("\n\t" + create_location_checkbox_(f"`at:{self.location}`")) if self.location != "" else ""
-        organiser = ("\n\t" + create_star_checkbox_(f"`host:{self.organiser}`")) if self.organiser != "" else ""
+        location = (
+            ("\n\t" + create_location_checkbox_(f"`at:{self.location}`"))
+            if self.location != ""
+            else ""
+        )
+        organiser = (
+            ("\n\t" + create_star_checkbox_(f"`host:{self.organiser}`"))
+            if self.organiser != ""
+            else ""
+        )
         return create_checkbox_(f"{str(self)}{location}{organiser}", checkbox_type)
