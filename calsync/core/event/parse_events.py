6e127ae7215948c68840e9b7261e359086fd97d6
Andrew Li <andrewli@u.northwestern.edu>
2025-10-24 19:14:24 -0500

    hide all day events from calsync

--- calsync/core/event/parse_events.py
+++ calsync/core/event/parse_events.py
@@ -9,12 +9,20 @@ logger = create_logger()
 
 class ParseEvents:
     def __init__(self, events: list[icalendar.Event]):
-        self.events = [self.parse_event(e) for e in events]
+        self.events = [pe for e in events if (pe := self.parse_event(e)) is not None]
 
-    def parse_event(self, e: icalendar.Event) -> event.T:
+    def parse_event(self, e: icalendar.Event) -> event.T | None:
         event_name = e.get("SUMMARY", "(untitled)")
         dtstart = arrow.get(e.get("DTSTART").dt).to("local")
         dtend = arrow.get(e.get("DTEND").dt).to("local")
+
+        param_value = e.get("DTSTART").params.get("VALUE", None)
+
+        # Hard coded to ignore all day events
+        # TODO: Make more robust
+        is_all_day_event = param_value is not None and param_value == "DATE"
+        if is_all_day_event:
+            return None
 
         # Hard coded for emails
         organiser = e.get("ORGANIZER", "")
